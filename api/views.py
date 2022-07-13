06f5bf879e856c82cbf1bf9bb10c274a198d01a8
amrakbari <akbariamirreza2001@gmail.com>
2022-07-13 17:32:37 +0430

    Room create api implemented

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializers
 
-# Create your views here.
+
+class RoomCreateView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializers
