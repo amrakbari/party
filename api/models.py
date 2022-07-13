753219bb60f6d587554f65238472404ac4ce92a2
amrakbari <akbariamirreza2001@gmail.com>
2022-07-13 17:59:38 +0430

    bug fixed on room code generation

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 import hashlib
+from datetime import datetime
 
 from django.db import models
 
@@ -9,7 +10,11 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    code = models.CharField(max_length=255, unique=True, default="")
 
-    @property
-    def code(self):
-        return f'{hashlib.md5(self.host).hexdigest()}_{self.created_at}'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.code = self.get_code()
+
+    def get_code(self):
+        return f'{hashlib.md5(self.host.encode("utf-8")).hexdigest()}_{datetime.utcnow().timestamp()}'
