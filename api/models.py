f40a231ac8afbc1b58defe9a75ac5c7230de06bf
amrakbari <akbariamirreza2001@gmail.com>
2022-07-13 17:12:50 +0430

    model Room created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
+import hashlib
+
 from django.db import models
 
+
 # Create your models here.
+class Room(models.Model):
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def code(self):
+        return f'{hashlib.md5(self.host).hexdigest()}_{self.created_at}'
